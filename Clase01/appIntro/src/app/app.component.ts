fc8f75c1b5ef9dfe25debff72e6346f588fea1a2
Alonzo <educatech1gmail.com>
2019-02-21 21:33:53 -0500

    Intro

--- Clase01/appIntro/src/app/app.component.ts
+++ Clase01/appIntro/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appIntro';
+
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer){
+    this.matIconRegistry.addSvgIcon(
+      "mi_icono",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("/assets/svg/china.svg")
+    )
+  }
+
 }
--- Clase01/appIntro/src/app/app.module.ts
+++ Clase01/appIntro/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatCardModule, MatSidenavModule, MatIconModule} from "@angular/material"
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent
@@ -14,7 +15,8 @@ import { MatButtonModule, MatCardModule, MatSidenavModule, MatIconModule} from "
     MatButtonModule,
     MatCardModule,
     MatSidenavModule,
-    MatIconModule
+    MatIconModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
