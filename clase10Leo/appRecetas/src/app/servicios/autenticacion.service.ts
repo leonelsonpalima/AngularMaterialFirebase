8f8114dff4bf481f9aa8364992223d9761648a4d
kalosk <mega.korus@gmail.com>
2019-04-04 22:06:03 -0500

    clase 10

---
+++ clase10Leo/appRecetas/src/app/servicios/autenticacion.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Usuario } from '../interfaces/usuario';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AutenticacionService {
+  onCambioEstado: Subject<boolean> = new Subject<boolean>()
+
+  constructor(private router: Router, private afAuth: AngularFireAuth) { }
+
+  login(usuario: Usuario) {
+    this.afAuth.auth.signInWithEmailAndPassword(usuario.correo, usuario.contrasena)
+      .then(
+        respuesta => {
+          this.onCambioEstado.next(true)
+          sessionStorage.setItem("usuario", JSON.stringify(usuario))
+          this.router.navigate(["dashboard"])
+        }
+      )
+
+  }
+
+  loginGoogle(){
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider())
+    .then(
+      data => {
+        console.log(data)
+      }
+    )
+  }
+
+  logout() {
+    this.afAuth.auth.signOut()
+    /*     this.onCambioEstado.next(false)
+        sessionStorage.clear()
+        this.router.navigate([""]) */
+  }
+
+  estaAutenticado(): boolean {
+    if (sessionStorage.getItem("usuario")) return true
+    return false
+  }
+}
